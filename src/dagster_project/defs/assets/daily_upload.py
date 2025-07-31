ccf58b2fb2b0d84ed4b5f353146d129b5b6dc21b
Bijil Subhash <bijil.subhash@gmail.com>
2025-07-31 18:23:41 +1000

    fixing get_date_formatted method

--- src/dagster_project/defs/assets/daily_upload.py
+++ src/dagster_project/defs/assets/daily_upload.py
@@ -12,7 +12,10 @@ from dagster_project.constants import (
 
 
 def _get_date_formatted(context: dg.AssetExecutionContext) -> str:
-    return datetime.strptime(context.partition_key, "%Y-%m-%d").strftime("%d-%m-%Y")
+    try:
+        return datetime.strptime(context.partition_key, "%Y-%m-%d").strftime("%d-%m-%Y")
+    except ValueError:
+        return datetime.now().strftime("%d-%m-%Y")
 
 
 @dg.asset(group_name="sftp")
