be045d07b55273e87058cd493b8a98c7f38bc27f
Bijil Subhash <bijil.subhash@gmail.com>
2025-08-02 18:58:49 +1000

    bugfix

--- src/dagster_project/defs/schedules.py
+++ src/dagster_project/defs/schedules.py
@@ -4,6 +4,5 @@ from dagster_project.defs.jobs import daily_upload_job
 daily_upload_schedule = dg.build_schedule_from_partitioned_job(
     job=daily_upload_job,
     hour_of_day=1,
-    execution_timezone="Australia/Sydney",
     default_status=dg.DefaultScheduleStatus.RUNNING,
 )
