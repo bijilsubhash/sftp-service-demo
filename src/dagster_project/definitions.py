5ef9f33a9f33bfc1e62671361627cff4dee9b47e
Bijil Subhash <bijil.subhash@gmail.com>
2025-08-02 19:42:43 +1000

    remove sensor

--- src/dagster_project/definitions.py
+++ src/dagster_project/definitions.py
@@ -2,11 +2,9 @@ import dagster as dg
 
 from dagster_project.defs.jobs import daily_upload_job
 from dagster_project.defs.schedules import daily_upload_schedule
-from dagster_project.defs.sensor import sensors
 
 defs = dg.Definitions(
     assets=dg.load_assets_from_package_name("dagster_project.defs.assets"),
     jobs=[daily_upload_job],
     schedules=[daily_upload_schedule],
-    sensors=sensors,
 )
