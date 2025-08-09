312707e9733e3e74339fcb123f455bc80dc55a43
Bijil Subhash <bijilsubhash@MacBook-Air.local>
2025-08-10 00:50:22 +1000

    changing asset names

--- src/main.py
+++ src/main.py
@@ -14,16 +14,18 @@ import dagster as dg
 logger = Logger(__name__)
 
 
+
+
 def upload_data(
     csv_file: str, data_dir: Path | None = None, date: str | None = None
 ) -> int:
-    row_count = 0
     sftp_client = SFTPClient(
-    hostName=dg.EnvVar("hostName").get_value(),
-    port=dg.EnvVar("port").get_value(),
-    userName=dg.EnvVar("userName").get_value(),
-    password=dg.EnvVar("password").get_value(),
-)
+        hostName=dg.EnvVar("hostName").get_value(),
+        port=dg.EnvVar("port").get_value(),
+        userName=dg.EnvVar("userName").get_value(),
+        password=dg.EnvVar("password").get_value(),
+    )
+    row_count = 0
     if date:
         date_formatted = datetime.strptime(date, "%d-%m-%Y").strftime("%Y%m%d")
         remote_path = f"input/{date_formatted}/{csv_file}"
